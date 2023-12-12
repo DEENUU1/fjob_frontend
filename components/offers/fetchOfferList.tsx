ca586e8abecf8c350eae25383a22ce4c5be69243
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-13 00:53:28 +0100

    Add function to process and display salary

--- components/offers/fetchOfferList.tsx
+++ components/offers/fetchOfferList.tsx
@@ -1,8 +1,8 @@
 import getApiUrl from "@/components/api";
-import { BiDotsVerticalRounded } from "react-icons/bi";
+import {BiDotsVerticalRounded} from "react-icons/bi";
 import Link from "next/link";
-import { FaHeart } from "react-icons/fa";
-import { FaFlag } from "react-icons/fa";
+import {FaHeart} from "react-icons/fa";
+import {FaFlag} from "react-icons/fa";
 
 async function getOffers() {
     // imitate delay from API
@@ -10,7 +10,7 @@ async function getOffers() {
 
     const response = await fetch(`${getApiUrl()}api/offer/offer`, {
         next: {
-             revalidate: 0
+            revalidate: 0
         }
     });
 
@@ -20,7 +20,7 @@ async function getOffers() {
 type Dict = Record<number, any>;
 
 
-function getDetails(offer: any) : Array<string> {
+function getDetails(offer: any): Array<string> {
     const experienceList: Array<Dict> = offer.experience;
     const workTypeList: Array<Dict> = offer.work_type;
     const employmentTypeList: Array<Dict> = offer.experience;
@@ -73,6 +73,31 @@ function getLocalization(offer: any): string {
 }
 
 
+interface Salary {
+    salaryFrom?: number,
+    salaryTo?: number,
+    currency?: string,
+    schedule?: string
+
+}
+
+function getSalary(offer: any): string {
+    const salaryList: Array<Salary> = offer.salary;
+    const numOfData: number = salaryList.length;
+
+    if (numOfData === 0) {
+        return '';
+    }
+    else if (numOfData === 1) {
+        const firstSalary = salaryList[0];
+        return `${firstSalary.salary_from} - ${firstSalary.salary_to} ${firstSalary.currency}/${firstSalary.schedule}`;
+    }
+    else {
+        return `${salaryList[0].salary_from} - ${salaryList[0].salary_to} ${salaryList[0].currency}/${salaryList[0].schedule} + ${numOfData - 1} salaries`;
+    }
+}
+
+
 function getCreatedTime(offer: any): string {
     const createdTime = offer.created_at;
     return createdTime.slice(0, 10);
@@ -80,19 +105,22 @@ function getCreatedTime(offer: any): string {
 }
 
 
-export default async function OfferList(){
+export default async function OfferList() {
     const offers = await getOffers();
 
     console.log(offers)
     return (
         <>
             {offers.results.map((offer: any) => (
-                <div className="border-2 border-3-black hover:border-black hover:border-3 rounded-2xl container mb-4 mt-4" key={offer.id}>
+                <div
+                    className="border-2 border-3-black hover:border-black hover:border-3 rounded-2xl container mb-4 mt-4"
+                    key={offer.id}>
                     <div className="flex justify-between items-center p-4">
                         <div className="flex items-start">
                             <div className="mr-4">
                                 <h2 className="text-3xl font-medium">{offer.title}</h2>
                                 <p className="text-base">{offer.company_name}</p>
+                                <h4 className="text-xl font-bold">{getSalary(offer)}</h4>
                             </div>
                         </div>
 
@@ -102,10 +130,10 @@ export default async function OfferList(){
 
                                 <div className="flex items-center space-x-4">
                                     <Link href="/" className="text-xl transform transition-transform hover:scale-110">
-                                        <FaFlag />
+                                        <FaFlag/>
                                     </Link>
                                     <Link href="/" className="text-xl transform transition-transform hover:scale-110">
-                                        <FaHeart />
+                                        <FaHeart/>
                                     </Link>
                                 </div>
                             </div>
@@ -115,13 +143,14 @@ export default async function OfferList(){
 
                     <div className="mb-3">
                         {getDetails(offer).map((detail: string) => (
-                            <span className="bg-black text-white font-medium rounded-2xl ml-4 py-2 px-2" key={detail}>{detail}</span>
+                            <span className="bg-black text-white font-medium rounded-2xl ml-4 py-2 px-2"
+                                  key={detail}>{detail}</span>
                         ))}
                     </div>
 
 
                     <div className="mb-3 p-4">
-                        <p>{offer.description}</p>
+                        <p>{offer.description.slice(0, 100)}...</p>
                     </div>
 
                     <div className="p-4 flex items-center justify-between">
@@ -138,7 +167,7 @@ export default async function OfferList(){
                                 href="/"
                                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                             >
-                                Report
+                                Apply
                             </Link>
                         </div>
                     </div>
