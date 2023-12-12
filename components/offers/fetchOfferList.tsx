c80ea672e9ecfc40f1e26b86a1c8516d8c50b585
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-12 22:47:38 +0100

    Card with job offer, header seaction

--- components/offers/fetchOfferList.tsx
+++ components/offers/fetchOfferList.tsx
@@ -1,8 +1,9 @@
 import getApiUrl from "@/components/api";
+import { BiDotsVerticalRounded } from "react-icons/bi";
 
 async function getOffers() {
     // imitate delay from API
-    await new Promise(resolve => setTimeout(resolve, 3000));
+    // await new Promise(resolve => setTimeout(resolve, 3000));
 
     const response = await fetch(`${getApiUrl()}api/offer/offer`, {
         next: {
@@ -14,13 +15,54 @@ async function getOffers() {
 }
 
 
+
 export default async function OfferList(){
     const offers = await getOffers();
     console.log(offers)
     return (
         <>
             {offers.results.map((offer: any) => (
-                <div key={offer.id}>{offer.title}</div>
+                <div className="border-2 border-red-900" key={offer.id}>
+                    <div className="flex justify-between items-center border p-4">
+                        <div className="flex items-start">
+                            <div className="mr-4">
+                                <h2 className="text-3xl font-medium">{offer.title}</h2>
+                                <p className="text-base">{offer.company_name}</p>
+                            </div>
+                        </div>
+
+                        <div className="flex items-center">
+
+                            <div className="relative inline-block text-left">
+                                <button className="text-2xl py-2 px-4 rounded-full focus:outline-none">
+                                    <BiDotsVerticalRounded />
+                                </button>
+                            {/*  Save & Report job offer */}
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div>
+                        <span>XXX</span>
+                        <span>XYZ</span>
+                    </div>
+
+
+                    <div>
+                        <p>{offer.description}</p>
+                    </div>
+
+                    <div>
+                        <div>
+                            <span> Localization </span>
+                            <span> Created time, website</span>
+                        </div>
+                        <div>
+                        {/*  Button to apply  */}
+                        </div>
+                    </div>
+                </div>
             ))}
         </>
     )
