44b5dcf30183b5a009aee109adef870bab2a4147
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-14 00:49:53 +0100

    Add Offer component to display title of a given offer

--- components/offers/fetchOffer.tsx
+++ components/offers/fetchOffer.tsx
@@ -1,7 +1,7 @@
 import getApiUrl from "@/components/api";
 
 
-export async function getOffer(id: number) {
+async function getOffer(id: number) {
     const response = await fetch(`${getApiUrl()}/api/offer/offer/${id}`, {
         next: {
             revalidate: 0
@@ -9,4 +9,23 @@ export async function getOffer(id: number) {
     });
 
     return response.json();
+}
+
+
+export default async function Offer(id: number){
+    const offer = await getOffer(id);
+
+    return (
+        <>
+            <div
+                className="border-2 border-3-black hover:border-black hover:border-3 rounded-2xl container mb-4 mt-4"
+            >
+
+                <h1>{offer.title}</h1>
+
+            </div>
+
+
+        </>
+    )
 }
