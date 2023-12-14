cf69089ff9f989e2592b967e480c4988942ee9fe
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-14 16:30:51 +0100

    Create function to send POST request with report

--- components/offers/report.tsx
+++ components/offers/report.tsx
@@ -1,6 +1,36 @@
+'use client'
+
 import {useState} from "react";
 import {FaFlag} from "react-icons/fa";
+import getApiUrl from "@/components/api";
+import { toast } from 'react-toastify';
 
 
 // <FaFlag/>
 
+const onSend = (description: string, offerId: number, userId: number, ) => {
+    const token = "todo"
+
+    const handlePostReport = async () => {
+        try {
+            const response = await fetch(`${getApiUrl()}/api/report`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify({"offer": offerId, "user": userId, "description": description})
+            })
+
+            if (response.status == 201) {
+                toast.success('Report sent')
+            } else {
+                toast.error('Something went wrong')
+            }
+
+        } catch (error) {
+            console.log(error)
+            toast.error('Something went wrong')
+        }
+    }
+}
