af97fe52a8409f62a1d421b66e64607de7d2d969
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-15 02:23:22 +0100

    Add sorting by date and price

--- app/offer/page.tsx
+++ app/offer/page.tsx
@@ -13,11 +13,18 @@ export default function Offers() {
     const [error, setError] = useState(null);
     const [isRemote, setIsRemote] = useState("");
     const [isHybrid, setIsHybrid] = useState("");
-    console.log(isRemote);
-    console.log(isHybrid);
+    const [ordering, setOrdering] = useState("created_at");
+    console.log(ordering);
+
+    const orderingTypes = new Map();
+    orderingTypes.set("Newest", "created_at");
+    orderingTypes.set("Oldest", "-created_at");
+    orderingTypes.set("Lowest salary", "salary__salary_from");
+    orderingTypes.set("Highest salary", "-salary__salary_from");
+
 
     useEffect(() => {
-        fetch(`${getApiUrl()}api/offer/offer?is_remote=${isRemote}&is_hybrid=${isHybrid}`)
+        fetch(`${getApiUrl()}api/offer/offer?is_remote=${isRemote}&is_hybrid=${isHybrid}&ordering=${ordering}`)
             .then(response => response.json())
             .then(data => {
                 setOffers(data);
@@ -27,7 +34,7 @@ export default function Offers() {
                 setError(error);
                 setLoading(false);
             });
-    }, [isRemote, isHybrid]);
+    }, [isRemote, isHybrid, ordering]);
 
     if (loading) {
         return <Loading/>;
@@ -54,6 +61,13 @@ export default function Offers() {
             <label htmlFor="is_hybrid" className="mr-2">Is hybrid:</label>
             <input type="checkbox" id="is_hybrid" checked={isHybrid === "true"} onChange={(e) => setIsHybrid(e.target.checked ? "true" : "") }/>
 
+
+            <label htmlFor="ordering">Sort by:</label>
+            <select id="ordering" value={ordering} onChange={(e) => setOrdering(e.target.value)}>
+                {Array.from(orderingTypes.keys()).map((key) => (
+                    <option key={key} value={orderingTypes.get(key)}>{key}</option>
+                ))}
+            </select>
 
             <h1 className="text-center font-bold text-3xl mb-10">Job offers list</h1>
             {offers.results.map((offer: any) => (
