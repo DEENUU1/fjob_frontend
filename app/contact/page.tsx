b83b37ab22f79502c7586038f1115aaf959e4857
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-15 22:53:25 +0100

    Change style for Contact form

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -4,8 +4,10 @@ export default function Contact() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-            <h1 className="text-center font-bold text-3xl">Contact form</h1>
-            <ContactForm/>
+            <div>
+                <h1 className="text-center font-bold text-3xl">Contact form</h1>
+                <ContactForm/>
+            </div>
         </main>
     )
 }
