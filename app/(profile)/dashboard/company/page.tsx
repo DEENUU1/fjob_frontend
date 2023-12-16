631fc9ceff31f7e40c2c1779529f7ffb37c962da
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-16 11:58:52 +0100

    Change Loading to Spinner component for all pages

--- app/(profile)/dashboard/company/page.tsx
+++ app/(profile)/dashboard/company/page.tsx
@@ -1,12 +1,12 @@
 import NumberOfCompaniesToCreate from '@/components/profile/NumOfCompaniesToCreate';
 import {Suspense} from "react";
-import Loading from "@/components/loading";
+import Spinner from '@/components/common/Spinner';
 
 
 export default async function UserCompanyDashboard() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <Suspense fallback={<Loading/>}>
+            <Suspense fallback={<Spinner/>}>
                 <NumberOfCompaniesToCreate/>
             </Suspense>
         </main>
