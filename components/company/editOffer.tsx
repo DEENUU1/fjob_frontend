978a0c198d2ab82c09ed3edb35fcae5c12d1570a
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-25 20:44:46 +0100

    Bugfix with duplicated props

--- components/company/editOffer.tsx
+++ components/company/editOffer.tsx
@@ -173,7 +173,7 @@ export default function EditOfferModal({offer}) {
                                     <div className="grid gap-6 mb-6 md:grid-cols-2 mt-2">
                                         <div className="flex items-center">
                                             <input value={isRemote} onChange={() => setIsRemote(!isRemote)}
-                                                   id="link-checkbox" type="checkbox" value=""
+                                                   id="link-checkbox" type="checkbox"
                                                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                                             <label htmlFor="link-checkbox" className="ms-2 text-sm font-medium">Is
                                                 remote</label>
@@ -181,7 +181,7 @@ export default function EditOfferModal({offer}) {
 
                                         <div className="flex items-center">
                                             <input value={isHybrid} onChange={() => setIsHybrid(!isHybrid)}
-                                                   id="link-checkbox" type="checkbox" value=""
+                                                   id="link-checkbox" type="checkbox"
                                                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                                             <label htmlFor="link-checkbox" className="ms-2 text-sm font-medium">Is
                                                 hybrid</label>
