588d1f07e5f616de206dad61736fad3488044de7
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-25 21:39:47 +0100

    Bugfix delete value from input checkbox

--- components/company/createOffer.tsx
+++ components/company/createOffer.tsx
@@ -139,12 +139,12 @@ export default function CreateJobOffer() {
 
                 <div className="grid gap-6 mb-6 md:grid-cols-2 mt-2">
                     <div className="flex items-center">
-                        <input value={isRemote} onChange={() => setIsRemote(!isRemote)} id="link-checkbox" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                        <input onChange={() => setIsRemote(!isRemote)} id="link-checkbox" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                         <label htmlFor="link-checkbox" className="ms-2 text-sm font-medium">Is remote</label>
                     </div>
 
                     <div className="flex items-center">
-                        <input value={isHybrid} onChange={() => setIsHybrid(!isHybrid)} id="link-checkbox" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+                        <input onChange={() => setIsHybrid(!isHybrid)} id="link-checkbox" type="checkbox" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
                         <label htmlFor="link-checkbox" className="ms-2 text-sm font-medium">Is hybrid</label>
                     </div>
                 </div>
