b22eb59c8b2c2da386880f9f693093fd9178806a
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-28 00:59:42 +0100

    Add page to display list of products

---
+++ app/product/page.tsx
@@ -0,0 +1,28 @@
+
+
+import {Suspense} from "react";
+import Spinner from "@/components/common/Spinner";
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: 'FJob | Products',
+}
+
+function Page() {
+
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="">
+                <h1 className="text-center font-bold text-3xl mb-10">Products</h1>
+
+                <Suspense fallback={<Spinner/>}>
+                    {/*<Companies/>*/}
+                </Suspense>
+
+            </div>
+        </main>
+    );
+}
+
+export default Page;
