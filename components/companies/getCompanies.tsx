c500a77054c87c8548e911dba7b4309cbed5ba6b
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-28 01:43:01 +0100

    Bugfix responsive Companies card

--- components/companies/getCompanies.tsx
+++ components/companies/getCompanies.tsx
@@ -19,27 +19,26 @@ export default async function Companies() {
         <>
             <div>
                 {data ? (
-                    <div className="grid grid-cols-3 gap-3">
+                    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
                         {data.map((company: any) => (
                             <div key={company.name}
                                  className="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
                                 <Link href={`/companies/${company.id}`}>
                                     {company.logo ? (
                                         <Image className="rounded-t-lg" src="/" alt={company.name} width={500}
-                                               height={500}/>
+                                               height={500} />
                                     ) : (
                                         <Image className="rounded-t-lg"
                                                src="https://www.eclosio.ong/wp-content/uploads/2018/08/default.png"
-                                               alt="default" width={500} height={500}/>
+                                               alt="default" width={500} height={500} />
                                     )}
 
                                     <div className="p-5">
-                                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{company.name}</h5>
-                                        <p className="mb-3 font-normal text-gray-700">{company.description.substring(0, 50) + "..."}</p>
+                                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 whitespace-normal line-clamp-2">{company.name}</h5>
+                                        <p className="mb-3 font-normal text-gray-700 line-clamp-3">{company.description}</p>
                                     </div>
                                 </Link>
                             </div>
-
                         ))}
                     </div>
                 ) : (
