be17f4441b0f71c1c6363cd140b1e97120c70a28
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-28 01:07:22 +0100

    Implement Products component to return list of grid cards

--- components/product/ProductList.tsx
+++ components/product/ProductList.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import Image from "next/image";
 
 
 async function FetchProducts() {
@@ -6,3 +8,30 @@ async function FetchProducts() {
 }
 
 
+export default async function Products(){
+    const data = await FetchProducts();
+
+    return (
+        <>
+            <div>
+                {data ? (
+                    <div className="grid grid-cols-3 gap-3">
+                        {data.map((product: any) => (
+                            <div key={product.name}
+                                 className="max-w-sm bg-white border border-gray-200 rounded-lg shadow">
+
+                                    <div className="p-5">
+                                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">{product.name}</h5>
+                                    </div>
+                            </div>
+
+                        ))}
+                    </div>
+                ): (
+                    <div>There is no products to display</div>
+                )}
+            </div>
+
+        </>
+    )
+}
