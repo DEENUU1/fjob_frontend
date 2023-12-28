d8dbe82108d21d32f43bce29d82e15a318b9bb40
DEENUU1 <kacperwlodarczyk@protonmail.com>
2023-12-28 01:46:40 +0100

    Fix responsive of contactForm

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,12 +8,11 @@ export const metadata: Metadata = {
 
 export default function Contact() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+        <>
             <div>
                 <h1 className="text-center font-bold text-3xl">Contact form</h1>
                 <ContactForm/>
             </div>
-        </main>
+        </>
     )
 }
