395d5416c9dd1a3d5c38d1fac52292f85af4cded
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-05 02:56:17 +0100

    Create CandidateFilter component

---
+++ components/company/CandidateFilter.tsx
@@ -0,0 +1,62 @@
+import React, {Dispatch, SetStateAction} from "react";
+
+
+export default function CandidateFilter(
+    {
+        ordering,
+        status,
+        setOrdering,
+        setStatus,
+    }:{
+        ordering: string,
+        status: string,
+        setOrdering: Dispatch<SetStateAction<string>>,
+        setStatus: Dispatch<SetStateAction<string>>,
+    }){
+
+    const orderingTypes = new Map();
+    orderingTypes.set("Newest", "-created_at");
+    orderingTypes.set("Oldest", "created_at");
+
+    const statusType: string[] = ["PENDING", "ACCEPTED", "REJECTED"];
+
+    return (
+        <>
+            <div className="lg:flex lg:space-x-4 lg:items-center lg:mb-4">
+                <label htmlFor="ordering">
+                    Sort by:
+                </label>
+                <select
+                    className="w-full bg-gray-50 font-medium p-2"
+                    id="ordering"
+                    value={ordering}
+                    onChange={(e) => setOrdering(e.target.value)}
+                >
+                    {Array.from(orderingTypes.keys()).map((key) => (
+                        <option key={key} value={orderingTypes.get(key)}>
+                            {key}
+                        </option>
+                    ))}
+                </select>
+
+                <label htmlFor="status">
+                    Status
+                </label>
+                <select
+                    className="w-full bg-gray-50 font-medium p-2"
+                    id="status"
+                    value={status}
+                    onChange={(e) => setStatus(e.target.value)}
+                >
+                    {statusType.map((statusOption) => (
+                        <option key={statusOption} value={statusOption}>
+                            {statusOption}
+                        </option>
+                    ))}
+                </select>
+
+            </div>
+        </>
+    )
+
+}
