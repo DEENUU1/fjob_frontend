2b45b5f4c9a23650d34de1168e90db0fb29e3132
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-07 00:34:55 +0100

    Bugfix Offerdetails

--- components/offer/FetchOffer.tsx
+++ components/offer/FetchOffer.tsx
@@ -64,16 +64,16 @@ export default async function OfferDetails({slug}: {slug: string}) {
                                 Apply
                             </Link>
                         )}
-                        {!isScrapedOffer && hasForm ? (
+                        {!isScrapedOffer && !hasForm ? (
                             <Link
                                 href={offer.apply_form}
                                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-bold rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                             >
                                 Apply
                             </Link>
-                        ) : (!isScrapedOffer && !hasForm && (
+                        ) : (!isScrapedOffer && hasForm && (
                             <Link
-                                href="/"
+                                href={`/offer/apply/${offer.id}`}
                                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-bold rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                             >
                                 Apply
