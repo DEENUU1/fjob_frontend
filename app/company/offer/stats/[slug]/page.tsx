08a9d1c0afe7c79ee366c833f154b8fd33561e00
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-08 17:22:07 +0100

    Add chart to display number of rate from 1 to 5

--- app/company/offer/stats/[slug]/page.tsx
+++ app/company/offer/stats/[slug]/page.tsx
@@ -16,13 +16,18 @@ export default function Page({params}: {params: PageParams}) {
 
     return (
         <>
-            <Suspense fallback={<Spinner/>}>
-                <CandidateStat offerId={offerId}/>
-            </Suspense>
+            <main className="mx-auto max-w-7xl py-6 my-8 sm:px-6 lg:px-8">
 
-            <Suspense fallback={<Spinner/>}>
-               <RateStats slug={offerSlug}/>
-            </Suspense>
+                <h2 className="text-xl">Candidates stats</h2>
+                <Suspense fallback={<Spinner/>}>
+                    <CandidateStat offerId={offerId}/>
+                </Suspense>
+
+                <h2 className="text-xl">Offer rate stats</h2>
+                <Suspense fallback={<Spinner/>}>
+                    <RateStats slug={offerSlug}/>
+                </Suspense>
+            </main>
         </>
     )
 }
--- components/company/OfferRateStat.tsx
+++ components/company/OfferRateStat.tsx
@@ -16,20 +16,90 @@ async function getOfferRateStats(slug: string){
 }
 
 
+function getPercentValue(value: number, sum: number) {
+    return (value / sum) * 100;
+}
+
 export default async function RateStats({slug}: {slug: string}){
     const offerRateStats = await getOfferRateStats(slug);
 
+    const fiveRateBar = getPercentValue(offerRateStats.five_rate, offerRateStats.num_rates);
+    const fourRateBar = getPercentValue(offerRateStats.four_rate, offerRateStats.num_rates);
+    const threeRateBar = getPercentValue(offerRateStats.three_rate, offerRateStats.num_rates);
+    const twoRateBar = getPercentValue(offerRateStats.two_rate, offerRateStats.num_rates);
+    const oneRateBar = getPercentValue(offerRateStats.one_rate, offerRateStats.num_rates);
+
     return (
         <>
-            <p>
-                <span>Avg: {offerRateStats.avg}</span>
-                <span>Num rates: {offerRateStats.num_rates}</span>
-                <span>1: {offerRateStats.one_rate}</span>
-                <span>2: {offerRateStats.two_rate}</span>
-                <span>3: {offerRateStats.three_rate}</span>
-                <span>4: {offerRateStats.four_rate}</span>
-                <span>5: {offerRateStats.five_rate}</span>
-            </p>
+            <div className="max-w-full mx-4 py-6 sm:mx-auto sm:px-6 lg:px-8">
+                <div className="sm:flex sm:space-x-4">
+                    <div
+                        className="inline-block hover:shadow-xl align-bottom bg-white rounded-lg text-left overflow-hidden shadow transform transition-all mb-4 w-full sm:w-1/3 sm:my-8">
+                        <div className="bg-white p-5">
+                            <div className="sm:flex sm:items-start">
+                                <div className="text-center sm:mt-0 sm:ml-2 sm:text-left">
+                                    <h3 className="text-sm leading-6 font-medium text-gray-900">Average rate</h3>
+                                    <p className="text-3xl font-bold text-black">{offerRateStats.avg} / 5</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        className="inline-block hover:shadow-xl align-bottom bg-white rounded-lg text-left overflow-hidden shadow transform transition-all mb-4 w-full sm:w-1/3 sm:my-8">
+                        <div className="bg-white p-5">
+                            <div className="sm:flex sm:items-start">
+                                <div className="text-center sm:mt-0 sm:ml-2 sm:text-left">
+                                    <h3 className="text-sm leading-6 font-medium text-gray-900">Total rates</h3>
+                                    <p className="text-3xl font-bold text-black">{offerRateStats.num_rates}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div>
+                <div className="flex items-center">
+                    <span className="text-sm font-medium dark:text-blue-500">5
+                        star</span>
+                    <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+                        <div className={`h-5 bg-yellow-300 rounded`} style={{"width": `${fiveRateBar}%`}}></div>
+                    </div>
+                    <span className="text-sm font-medium text-gray-500 dark:text-gray-400">{fiveRateBar} %</span>
+                </div>
+                <div className="flex items-center mt-4">
+                    <span className="text-sm font-medium dark:text-blue-500">4
+                        star</span>
+                    <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+                        <div className="h-5 bg-yellow-300 rounded" style={{"width": `${fourRateBar}%`}}></div>
+                    </div>
+                    <span className="text-sm font-medium text-gray-500 dark:text-gray-400">{fourRateBar} %</span>
+                </div>
+                <div className="flex items-center mt-4">
+                    <span className="text-sm font-medium dark:text-blue-500">3
+                        star</span>
+                    <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+                        <div className="h-5 bg-yellow-300 rounded" style={{"width": `${threeRateBar}%`}}></div>
+                    </div>
+                    <span className="text-sm font-medium text-gray-500 dark:text-gray-400">{threeRateBar} %</span>
+                </div>
+                <div className="flex items-center mt-4">
+                    <span className="text-sm font-medium dark:text-blue-500">2
+                        star</span>
+                    <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+                        <div className="h-5 bg-yellow-300 rounded" style={{"width": `${twoRateBar}%`}}></div>
+                    </div>
+                    <span className="text-sm font-medium text-gray-500 dark:text-gray-400">{twoRateBar} %</span>
+                </div>
+                <div className="flex items-center mt-4">
+                    <span className="text-sm font-medium dark:text-blue-500">1
+                        star</span>
+                    <div className="w-2/4 h-5 mx-4 bg-gray-200 rounded dark:bg-gray-700">
+                        <div className="h-5 bg-yellow-300 rounded" style={{"width": `${oneRateBar}%`}}></div>
+                    </div>
+                    <span className="text-sm font-medium text-gray-500 dark:text-gray-400">{oneRateBar} %</span>
+                </div>
+            </div>
         </>
     )
 }
