6f39335fb592a2ca986451bfb0c7ac0469733f1a
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-10 02:20:05 +0100

    Change spinner to NextUI component

--- components/common/Spinner.tsx
+++ components/common/Spinner.tsx
@@ -1,23 +1,8 @@
-import cn from 'classnames';
-import { ImSpinner3 } from 'react-icons/im';
+import {Spinner as SpinnerNextUI} from "@nextui-org/react";
 
-interface Props {
-    sm?: boolean;
-    md?: boolean;
-    lg?: boolean;
-}
-
-export default function Spinner({ sm, md, lg }: Props) {
-    const className = cn('animate-spin text-white-300 fill-white-300 mr-2', {
-        'w-4 h-4': sm,
-        'w-6 h-6': md,
-        'w-8 h-8': lg,
-    });
+export default function Spinner() {
 
     return (
-        <div role='status'>
-            <ImSpinner3 className={className} />
-            <span className='sr-only'>Loading...</span>
-        </div>
+        <SpinnerNextUI/>
     );
 }
